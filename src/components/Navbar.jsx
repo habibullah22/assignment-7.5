1261072063b7a9d165e82978eed1b7cd7600cd5d
habibullah22 <habib.bd.pro.2025@gmail.com>
2025-12-10 21:42:28 +0600

    aded navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+   (
+    <div>
+      <nav className="px-8 py-4 flex justify-between items-center container mx-auto">
+        <h1 className="md:text-xl text-sm font-bold">CS — Ticket System</h1>
+
+       
+        <div className="md:flex hidden items-center gap-6">
+          <a href="#" className="text-gray-600">Home</a>
+          <a href="#" className="text-gray-600">FAQ</a>
+          <a href="#" className="text-gray-600">Changelog</a>
+          <a href="#" className="text-gray-600">Blog</a>
+          <a href="#" className="text-gray-600">Download</a>
+          <a href="#" className="text-gray-600">Contact</a>
+          <button className="bg-purple-600 text-white px-4 py-2 rounded-lg">
+            + New Ticket
+          </button>
+        </div>
+
+    
+    
+      </nav>
+
+    
+      {open && (
+        <div className="md:hidden px-8 pb-4 flex flex-col gap-3">
+          <a href="#" className="text-gray-600">Home</a>
+          <a href="#" className="text-gray-600">FAQ</a>
+          <a href="#" className="text-gray-600">Changelog</a>
+          <a href="#" className="text-gray-600">Blog</a>
+          <a href="#" className="text-gray-600">Download</a>
+          <a href="#" className="text-gray-600">Contact</a>
+          <button className="bg-purple-600 text-white px-4 py-2 rounded-lg w-fit">
+            + New Ticket
+          </button>
+        </div>
+      )}
+    </div>
+  ));
+};
+
+export default Navbar;
+<h1>Navbar</h1>;
